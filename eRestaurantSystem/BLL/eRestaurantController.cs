62a446662a55e3ecd2bc3f2c2ac8009ba0430567
kbulger2 <bulgerkeyen@gmail.com>
2014-10-03 08:31:56 -0600

    Download Instructor Example

---
+++ eRestaurantSystem/BLL/eRestaurantController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class eRestaurantController
+    {
+    #region SpecialEvents
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            //interfacing with our Context class
+            using(eRestaurantContext context = new eRestaurantContext())
+            {
+                // using Context DbSet to get entity data
+                //return context.SpecialEvents.ToList();
+
+                //get a list of instances for entity using LINQ
+                var results = from item in context.SpecialEvents
+                              select item;
+                return results.ToList();
+
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public SpecialEvent SpecialEventByEventCode(string eventcode)
+        {
+            using(eRestaurantContext context = new eRestaurantContext())
+            {
+                return context.SpecialEvents.Find(eventcode);
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public void SpecialEvents_Add(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                SpecialEvent added = null;
+                added = context.SpecialEvents.Add(item);
+                // commits the add to the database
+                // evaluates the annotations (validations) on your entity
+                // [Required],[StringLength],[Range],...
+                context.SaveChanges();  
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update,false)]
+        public void SpecialEvents_Update(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                context.Entry<SpecialEvent>(context.SpecialEvents.Attach(item)).State =System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete,false)]
+        public void SpecialEvents_Delete(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                SpecialEvent existing = context.SpecialEvents.Find(item.EventCode);
+                context.SpecialEvents.Remove(existing);
+                context.SaveChanges();
+            }
+        }
+    #endregion
+
+    #region Reservations
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Reservation> Reservation_List()
+        {
+            //interfacing with our Context class
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                return context.Reservations.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Reservation> ReservationbyEvent(string eventcode)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                return context.Reservations.Where(anItem => anItem.Eventcode == eventcode).ToList();
+            }
+        }
+    #endregion
+
+    }
+}
